12e2a1ef15bf56394e9e9187148ccd5c746acf6c
Atul Singh <37662918+singh-atul@users.noreply.github.com>
2018-09-06 15:07:17 +0530

    Add files via upload
    latest code

--- Class_global_path_coverage.py
+++ Class_global_path_coverage.py
@@ -13,15 +13,17 @@ import matplotlib.pyplot as plt
 
 class gloabl_path_coverage:
     
-    def __init__(self,fileName,lgt_upper_lim,lat_upper_lim,inter,data_sep=" "):
+    def __init__(self,fileName,output_file,lgt_upper_lim,lat_upper_lim,inter,data_sep=" "):
         self.gps_file_name = fileName
+        self.output_filepath_name = output_file
         self.longitude_upper_limit = lgt_upper_lim
         self.latitude_upper_limit = lat_upper_lim
+        self.limit = inter
         self.data_separator = data_sep
         self.list_boundaries = []
         self.list_polygons= []
-        self.moves=[[0,-inter],[-inter,0],[0,inter],[inter,0]]
-        
+        self.moves=[[0.,-inter ,'S'],[-inter,0.,'W'],[0.,inter,'N'],[inter,0.,'E']]
+        self.array_points = []
 
     def load_data(self):
         try:
@@ -30,7 +32,7 @@ class gloabl_path_coverage:
             dataframe = pd.read_csv(self.gps_file_name, sep=self.data_separator, header=None)
             
             #numpy array of shape nx2 of all points present in file
-            array_points  = dataframe.apply(pd.to_numeric, args=('coerce',)).values
+            self.array_points  = dataframe.apply(pd.to_numeric, args=('coerce',)).values
             
         except Exception as ex:
             print ("gloabl_path_coverage.load_data: Exception while reading data from file and converting to numpy array : " + str(ex))
@@ -42,7 +44,7 @@ class gloabl_path_coverage:
         try:
             
             tempBoundary = []
-            for point in array_points:
+            for point in self.array_points:
                 
                 # if value recieved in NAN then add in final list of boundaries
                 # else keep on cumulating the points in tempBoundary
@@ -66,9 +68,9 @@ class gloabl_path_coverage:
             for boundary in self.list_boundaries:
                 points = np.array(boundary)
                 self.list_polygons.append(Polygon(points))
-                
+                print("gloabl_path_coverage.prepare_polygon: Info : Processing complete with Number of Polygons as %s",str(len(self.list_polygons)))    
         except Exception as ex:
-            print ("gloabl_path_coverage.prepare_polygon---: Exception while preparing polygon list from boundary lists  : " + str(ex)) 
+            print ("gloabl_path_coverage.prepare_polygon: Exception while preparing polygon list from boundary lists  : " + str(ex)) 
             return False
         
         # in case no issues return true stating success
@@ -87,9 +89,10 @@ class gloabl_path_coverage:
     def prepare_edges(self):
         try:
             min_x,min_y,max_x,max_y=self.list_polygons[0].bounds
-            longitude = np.arange(min_x,max_x + self.longitude_upper_limit )
-            latitude = np.arange(min_y,max_y + self.latitude_upper_limit )
+            longitude = np.arange(min_x,max_x + self.longitude_upper_limit,self.limit )
+            latitude = np.arange(min_y,max_y + self.latitude_upper_limit ,self.limit)
             edges =[]
+            flag_poly = False
             for x in range(0,len(longitude)):
                 for y in range(0,len(latitude)):
                     point = Point([longitude[x],latitude[y]])
@@ -97,10 +100,17 @@ class gloabl_path_coverage:
                     # to check if point found is inside the parameter or on the lawn boundary 
                     # in that case check if it's not inside any of obstacle value
                     if self.list_polygons[0].contains(point) or np.any(np.equal([longitude[x],latitude[y]] , self.list_boundaries[0]).all(axis=1)):
-                        for obstacle_poly in self.list_polygons[1:]:
-                            if not obstacle_poly.contains(point):
+                        if ( len(self.list_polygons) > 1 ):
+                            flag_poly =True
+                            for obstacle_poly in self.list_polygons[1:]:
+                                if obstacle_poly.contains(point):
+                                    flag_poly=False
+                            if flag_poly:
                                 edges.append([longitude[x],latitude[y]])
+                        else:
+                            edges.append([longitude[x],latitude[y]])
             
+            print ("gloabl_path_coverage.prepare_edges : Info : Number of Lawn Boundary Points - %s ",str(len(edges)))
         except Exception as ex:
             print ("gloabl_path_coverage.prepare_edges : Can't process edges: " + str(ex))
             return []
@@ -109,22 +119,33 @@ class gloabl_path_coverage:
     #called by internal function , need not be called explicitly
     def move_next(self,curr_pos,traversed_list):
         try:
+            flag_poly=False
             for move in self.moves:
                 next_x = curr_pos[0] + move[0]
                 next_y = curr_pos[1] + move[1]
+                theta = move[2]
                 point = Point(next_x , next_y)
                 if [next_x , next_y] not in traversed_list:
                     ### check this logic
-                    if self.list_polygons[0].contains(point) or np.any(np.equal([next_x , next_y] , self.list_boundaries[0]).all(axis=1)):
+                    if self.list_polygons[0].contains(point) or np.any(np.equal([next_x , next_y] , self.array_points).all(axis=1)):
                         if( len(self.list_polygons) > 1):
+                            flag_poly=True
                             for obstacle_poly in self.list_polygons[1:]:
-                                if not obstacle_poly.contains(point):
-                                    return [next_x,next_y]
+                                if obstacle_poly.contains(point):
+                                    flag_poly=False
+                            if flag_poly:
+                                return [next_x,next_y,theta]
                         else:
-                            return [next_x,next_y]
+                            return [next_x,next_y,theta]
             return None
         except Exception as ex:
             print ("gloabl_path_coverage.move_next : Exception in calculating next move : " + str(ex))
+    
+    
+    def save_to_file(data):
+        with open(self.output_filepath_name, 'a') as f:
+        
+            f.write(data + '\n')
     
     #function
     #Returns : list of points to be traversed
@@ -132,18 +153,25 @@ class gloabl_path_coverage:
         
         try:
             edges = self.prepare_edges()
-            current_pos = edges[0]
+            current_pos = edges[0][:]
+            current_pos.extend('S')
             traversed_list = []
+            final_wapoint_list = []
 
             while len(edges)!=0 :
 
                 if current_pos == None:
-                    current_pos = edges[0]
-                edges.remove(current_pos)
-                traversed_list.append(current_pos)
+                    current_pos = edges[0][:]
+                    current_pos.extend('S')
+                point= current_pos[:2]
+                edges.remove(point)
+                traversed_list.append(current_pos[:2])
+                final_wapoint_list.append(current_pos[:2])
+                #self.save_to_file(current_pos[:2])
                 current_pos = self.move_next(current_pos,traversed_list)
-
-            return traversed_list
+            
+            print("gloabl_path_coverage.gen_traversedList: Info : Final wapoint list generated with length - %s ",str(len(final_wapoint_list)))
+            return final_wapoint_list
         except Exception as ex :
             print ("gloabl_path_coverage.gen_traversedList : Exception in generating traversed list : " + str(ex))
             return []
